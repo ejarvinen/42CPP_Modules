77e9abb6ee644692b31f9b609bed78a4ab209b43
ejarvinen <emmi.jarvinen@proton.me>
2024-10-07 14:15:41 +0300

    Finished CPP01ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:02:04 by emansoor          #+#    #+#             */
-/*   Updated: 2024/10/07 14:02:12 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/10/07 14:14:57 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,19 @@
 
 int	main(void)
 {
-	
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Memory address held by str: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << &*stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Value of variable str: " << str << std::endl;
+	std::cout << "Value of variable stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of variable stringREF: " << stringREF << std::endl;
+
 	return (0);
 }
