3a441ef23c630a4f5124a5056c259f299bc57f4f
ejarvinen <emmi.jarvinen@proton.me>
2024-10-25 15:03:36 +0300

    Added bureaucrat stats to tests

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:38:54 by emansoor          #+#    #+#             */
-/*   Updated: 2024/10/25 14:47:56 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/10/25 15:02:33 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(void)
 	Form		SeparationStatement("Separation Statement", 13, 1);
 	Form		MarriageLicense("Marriage License", 125, 1);
 
+	std::cout << Aurora;
+	std::cout << Cinderella;
 	std::cout << BirthAffidavit;
 	std::cout << SeparationStatement;
 	std::cout << MarriageLicense;
