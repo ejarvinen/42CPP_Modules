440833d2fe9faf7363379cadf3e7178f510f372a
ejarvinen <emmi.jarvinen@proton.me>
2024-11-19 10:27:48 +0200

    Added test case for string array

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,14 +6,17 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:54:55 by emansoor          #+#    #+#             */
-/*   Updated: 2024/11/18 17:10:06 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/11/19 10:23:41 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_H
 # define ITER_H
 
-template <typename T, typename W>
-void    iter(T *address, unsigned int len, W (*func)(T &param));
+// template <typename T, typename W>
+// void    iter(T *address, unsigned int len, W (*func)(T &param));
+
+template <typename T>
+void    iter(T *address, unsigned int len, void (*func)(T &param))
 
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,15 +6,18 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:59:32 by emansoor          #+#    #+#             */
-/*   Updated: 2024/11/18 17:12:07 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/11/19 10:27:24 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
+#include <string>
 
-template <typename T, typename W>
-void    iter(T *address, unsigned int len, W (*func)(T &param))
+// template <typename T, typename W>
+// void    iter(T *address, unsigned int len, W (*func)(T &param))
+template <typename T>
+void    iter(T *address, unsigned int len, void (*func)(T &param))
 {
 	unsigned int	index;
 
@@ -25,9 +28,16 @@ void    iter(T *address, unsigned int len, W (*func)(T &param))
 	}
 }
 
-void	increment(int &num)
+template <typename T>
+void	increment(T &member)
 {
-	num++;
+	member++;
+}
+
+template <typename T>
+void	printMember(T &member)
+{
+	std::cout << member << std::endl;
 }
 
 int	main(void)
@@ -37,12 +47,22 @@ int	main(void)
 		
 		for (int index = 0; index < 10; index++)
 			nums[index] = index;
+		iter(nums, 10, printMember);
 		for (int index = 0; index < 10; index++)
 			std::cout << "nums[" << index << "] = " << nums[index] << std::endl;
 		iter(nums, 10, increment);
+		std::cout << std::endl;
 		for (int index = 0; index < 10; index++)
 			std::cout << "nums[" << index << "] = " << nums[index] << std::endl;
 		delete[] nums;
+	}
+	std::cout << std::endl;
+	{
+		std::string	fruits[5] = {"apple", "banana", "coconut", "durian", "elderberry"};
+		std::string	*fruitPtr = &fruits[5];
+		
+		iter(fruitPtr, 5, printMember);
+		iter(fruitPtr, 5, increment);
 	}
 	return (0);
 }
