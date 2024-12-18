6e5fb01a92f73551cce8fb01f51cbcdd2b0d881f
ejarvinen <emmi.jarvinen@proton.me>
2024-12-18 11:14:37 +0200

    Cleaned up recursion; works with even numbers

--- CPP09/ex02/vector.cpp
+++ CPP09/ex02/vector.cpp
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 15:57:50 by emansoor          #+#    #+#             */
-/*   Updated: 2024/12/17 15:31:10 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/12/18 11:12:51 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,44 @@
 
 void	PmergeMe::arrangePairs(int elem1, int elem2, int N)
 {
-	std::vector<int>			temp;
-	std::vector<int>::iterator	it;
-	int							i = elem2 - N;
+	std::vector<int>	temp;
+	int					index = 0;
+	int					i = elem1 + 1;
 
-	std::advance(elem2, (-1) * (N - 1));
-	while (i < elem2)
+	while (i <= elem2)
 	{
-		temp.push_back(_sortedVec[elem2]);
-		std::advance(elem2, 1);
+		temp.push_back(_sortedVec[i]);
 		i++;
 	}
-	i = N / 2;
-	while (i > 0)
+	i = 0;
+	while (i < N / 2)
 	{
-		
+		_sortedVec[elem2 - i] = _sortedVec[elem1 - i];
+		i++;
 	}
-
-	
-
-	std::cout << "N: " << N << std::endl;
-	std::cout << "e1: " << *elem1 << std::endl;
-	std::cout << "e2: " << *elem2 << std::endl;
+	i--;
+	while (i >= 0)
+	{
+		_sortedVec[elem1 - i] = temp[index];
+		index++;
+		i--;
+	}
 }
 
 void	PmergeMe::sortVec(int level, int pairs)
 {
-	int	elem1;
-	int	elem2;
-	int							pair = 1;
-	int							temp;
-	double						N = pow(2, level);
+	int		elem1;
+	int		elem2;
+	int		pair = 1;
+	int		temp;
+	double	N = pow(2, level);
 	
 	elem2 = N - 1;
 	elem1 = 0;
 	if (level != 1)
 		elem1 = N - 1 - std::floor(N / 2);
-	std::cout << "level " << level << std::endl;
 	while (pair <= pairs)
 	{
-		std::cout << "elem1: " << _sortedVec[elem1] << std::endl;
-		std::cout << "elem2: " << _sortedVec[elem2] << std::endl;
 		if (_sortedVec[elem1] > _sortedVec[elem2])
 		{
 			if (level == 1)
@@ -74,8 +71,7 @@ void	PmergeMe::sortVec(int level, int pairs)
 	}
 	if (pairs / 2 >= 1)
 	{
-		level++;
-		sortVec(level, pairs / 2);
+		sortVec(++level, pairs / 2);
 	}
 	else
 	{
@@ -94,65 +90,7 @@ void	PmergeMe::sortVec(int level, int pairs)
 	}
 }
 
-/* void	PmergeMe::sortVec(int level, int pairs)
-{
-	std::vector<int>::iterator	elem1;
-	std::vector<int>::iterator	elem2;
-	int							pair = 1;
-	int							temp;
-	double						N = pow(2, level);
-	
-
-	elem1 = _sortedVec.begin();
-	elem2 = elem1;
-	std::advance(elem2, N - 1);
-	if (level != 1)
-		std::advance(elem1, N - 1 - std::floor(N / 2));
-	std::cout << "level " << level << std::endl;
-	while (pair <= pairs)
-	{
-		std::cout << "elem1: " << *elem1 << std::endl;
-		std::cout << "elem2: " << *elem2 << std::endl;
-		if (*elem1 > *elem2)
-		{
-			if (level == 1)
-			{
-				temp = *elem1;
-				*elem1 = *elem2;
-				*elem2 = temp;
-			}
-			else
-			{
-				//arrangePairs(elem1, elem2, N);
-			}
-		}
-		std::advance(elem2, N);
-		elem1 = elem2;
-		std::advance(elem1, (-1) * std::floor(N / 2));
-		pair++;
-	}
-	if (pairs / 2 >= 1)
-	{
-		level++;
-		sortVec(level, pairs / 2);
-	}
-	else
-	{
-		std::cout << "og sequence: " << std::endl;
-		for (int i = 1; _unsorted[i]; i++)
-			std::cout << _unsorted[i] << " ";
-		std::cout << std::endl;
-		std::cout << "sorted sequence: " << std::endl;
-		int i = 0;
-		while (i < _size)
-		{
-			std::cout << _sortedVec[i] << " ";
-			i++;
-		}
-		std::cout << std::endl;
-	}
-} */
-
+// even number:
 // ./PmergeMe 1 3 15 8 14 7 10 5 11 12 13 4 9 0 2 6
 
 // after level 1:
@@ -162,7 +100,11 @@ void	PmergeMe::sortVec(int level, int pairs)
 // 1 3 8 15 5 10 7 14 11 12 4 13 2 6 0 9
 
 // after level 3:
-// 5 10 7 14 1 3 8 15 2 6 0 9 11 12 4 18
+// 5 10 7 14 1 3 8 15 2 6 0 9 11 12 4 13
 
 // after level 4:
-// - || -
+// 2 6 0 9 11 12 4 13 5 10 7 14 1 3 8 15
+
+// even but not enough:
+
+// 
