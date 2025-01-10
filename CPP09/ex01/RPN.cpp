33b8f737f4fe386aaebbe1d970253e9fadb3f99c
ejarvinen <emmi.jarvinen@proton.me>
2025-01-10 08:53:05 +0200

    Made error messages more cohesive, added overflow protection

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 16:24:29 by emansoor          #+#    #+#             */
-/*   Updated: 2024/12/27 09:18:58 by emansoor         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:59:35 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ RPN	&RPN::operator=(RPN const &other)
 	return (*this);
 }
 
-bool	RPN::isOperand(std::string arg)
+int	RPN::isOperand(std::string arg)
 {
 	int	value;
 	
 	for (int index = 0; arg[index]; index++)
 	{
 		if (std::isdigit(arg[index]) == 0)
-			return (false);
+			return (2);
 	}
 	try
 	{
@@ -42,15 +42,15 @@ bool	RPN::isOperand(std::string arg)
 	}
 	catch (const std::exception& e)
 	{
-		std::cerr << "Error: number out of range" << std::endl;
-		return (false);
+		std::cerr << "Error: element not representable as int" << std::endl;
+		return (1);
 	}
 	if (value > 9 || value < 0)
 	{
 		std::cerr << "Error: number out of range" << std::endl;
-		return (false);
+		return (1);
 	}
-	return (true);
+	return (0);
 }
 
 bool	RPN::isOperator(std::string arg)
@@ -71,24 +71,55 @@ void	RPN::pushOntoStack(std::string arg)
 	_stack.push(operand);
 }
 
+bool	RPN::overflowCheck(int *res, int first, int scnd, int operation)
+{
+	if (operation == 1)
+	{
+		*res = scnd + first;
+		if (scnd > 0 && first > 0 && *res < 0)
+			return (false);
+	}
+	if (operation == 2)
+	{
+		*res = scnd - first;
+		if (scnd < 0 && first < 0 && *res > 0)
+			return (false);
+	}
+	if (operation == 3)
+	{
+		*res = first * scnd;
+		if (first > 0 && scnd > 0 && *res < 0)
+			return (false);
+	}
+	if (operation == 4)
+	{
+		*res = scnd / first;
+	}
+	return (true);
+}
+
 int	RPN::performOperation(std::string arg)
 {
-	int	first;
-	int	scnd;
-	int	res;
+	int		first;
+	int		scnd;
+	int		res;
+	bool	overflow;
 
 	if (_stack.empty() || _stack.size() < 2)
+	{
+		std::cerr << "Error: faulty input logic" << std::endl;
 		return (1);
+	}
 	first = _stack.top();
 	_stack.pop();
 	scnd = _stack.top();
 	_stack.pop();
 	if (arg.compare("+") == 0)
-		res = scnd + first;
+		overflow = overflowCheck(&res, first, scnd, 1);
 	else if (arg.compare("-") == 0)
-		res = scnd - first;
+		overflow = overflowCheck(&res, first, scnd, 2);
 	else if (arg.compare("*") == 0)
-		res = first * scnd;
+		overflow = overflowCheck(&res, first, scnd, 3);
 	else
 	{
 		if (first == 0)
@@ -96,7 +127,12 @@ int	RPN::performOperation(std::string arg)
 			std::cerr << "Error: division by zero" << std::endl;
 			return (1);
 		}
-		res = scnd / first;
+		overflow = overflowCheck(&res, first, scnd, 4);
+	}
+	if (!overflow)
+	{
+		std::cerr << "Error: result overflows" << std::endl;
+		return (1);
 	}
 	_stack.push(res);
 	return (0);
@@ -108,22 +144,28 @@ void	RPN::processExpression(std::string args)
 	std::string			arg;
 	int					argCount = 0;
 	char				del = ' ';
+	int					operandType;			
 	
 	while (getline(ss, arg, del))
 	{
-		if (isOperand(arg))
+		operandType = isOperand(arg);
+		if (operandType == 1)
+			return ;
+		if (operandType == 0)
 		{
 			pushOntoStack(arg);
 		}
-		else if (isOperator(arg))
+		else if (isOperator(arg) && operandType != 0) // check this condition
 		{
-			if (argCount == 0 || performOperation(arg))
+			if (argCount == 0)
 			{
 				std::cerr << "Error: faulty input logic" << std::endl;
 				return ;
 			}
+			if (performOperation(arg))
+				return ;
 		}
-		else
+		else if (operandType > 1) // and this
 		{
 			std::cerr << "Error: suspicious input" << std::endl;
 			return ;
--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 16:24:34 by emansoor          #+#    #+#             */
-/*   Updated: 2024/12/12 15:24:55 by emansoor         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:58:08 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ class RPN {
 
 		std::stack<int>	_stack;
 		
-		bool	isOperand(std::string arg);
+		int		isOperand(std::string arg);
 		bool	isOperator(std::string arg);
 		void	pushOntoStack(std::string arg);
 		int		performOperation(std::string arg);
+		bool	overflowCheck(int *res, int first, int scnd, int operation);
 
 	public:
 
